3418326cb7e0ddcd83956e8da5812902bebbbc8d
behzad1993 <karimi1993@live.de>
2023-10-25 19:14:21 +0200

    WIP: - adding controller, returning simplet string

---
+++ src/main/java/behzad/aws_ip_filter/controller/IpController.java
@@ -0,0 +1,26 @@
+package behzad.aws_ip_filter.controller;
+
+import behzad.aws_ip_filter.Region;
+import behzad.aws_ip_filter.service.IpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+@RestController
+@RequestMapping("api/v1")
+public class IpController {
+
+    @Autowired
+    IpService ipService;
+
+    @GetMapping(value = "/ips", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String findAllIpsByRegion(@RequestParam("region") Region region) throws URISyntaxException, IOException {
+        return ipService.getFilteredIps(region).toString();
+    }
+}
