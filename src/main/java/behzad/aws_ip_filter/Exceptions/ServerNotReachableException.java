326d9da6fa37628d34dfafdc2a28caed7fa435b2
behzad1993 <karimi1993@live.de>
2023-10-26 22:51:20 +0200

    - adding new exception type

---
+++ src/main/java/behzad/aws_ip_filter/Exceptions/ServerNotReachableException.java
@@ -0,0 +1,8 @@
+package behzad.aws_ip_filter.Exceptions;
+
+public class ServerNotReachableException extends Exception{
+
+    public ServerNotReachableException(String message) {
+        super(message);
+    }
+}
